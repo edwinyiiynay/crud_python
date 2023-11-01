3c33ce100d3b126ebee86cc89636de5d2480d84e
Jucarca <juditcarrilero@gmail.com>
2023-11-01 17:03:15 +0100

    Se agrega el crud

---
+++ tasks/views.py
@@ -0,0 +1,42 @@
+#importo las liobrerias
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.contrib.auth import login
+from django.db import IntegrityError
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def signup(request):
+
+    if request.method == 'GET':
+        return render(request, 'signup.html', {
+            'form': UserCreationForm
+        })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            # register user
+            try:
+                user = User.objects.create_user(
+                    username=request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('tasks')
+                return HttpResponse('Usuario creado correctamente')
+            except IntegrityError:
+                return render(request, 'signup.html', {
+                    'form': UserCreationForm,
+                    'error': 'El usuario uya existe'
+                })
+                render(request, 'signup.html', {
+                    'form': UserCreationForm,
+                    'error': 'No coninciden las contraseñas'
+                })
+
+def tasks(request):
+    return render(request, 'tasks.html')
